c1167aa1c0d21799da0471bb87b178e116aba72f
ahuerta <angeladrianhp@hotmail.com>
2021-12-17 06:33:36 -0600

    Fix class names

--- src/components/StepForm/ComfortData.jsx
+++ src/components/StepForm/ComfortData.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Form from './Form';
-
 function ComfortData() {
   return (
-    <Form>
+    <>
       <div className="mb-3">
         <label className="form-label">¿El departamento cuenta con algunas de las siguientes comodidades?:</label>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="chkBBQ" />
-          <label class="form-check-label" for="chkBBQ">Zona BBQ</label>
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="chkBBQ" />
+          <label className="form-check-label" htmlFor="chkBBQ">Zona BBQ</label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="chkSalon" />
-          <label class="form-check-label" for="chkSalon">Salón comunal</label>
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="chkSalon" />
+          <label className="form-check-label" htmlFor="chkSalon">Salón comunal</label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="chkJuegos" />
-          <label class="form-check-label" for="chkJuegos">Parque de juegos</label>
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="chkJuegos" />
+          <label className="form-check-label" htmlFor="chkJuegos">Parque de juegos</label>
         </div>
       </div>
       <div className="d-flex justify-content-between">
         <Link to="/register/department-floor" className="btn btn-sm btn-primary">Anterior</Link>
         <Link to="/register/department-parking" className="btn btn-sm btn-primary">Siguiente</Link>
       </div>
-    </Form>
+    </>
   )
 }
 
