9c79d70920468afb6f48a24600b0caa1a7958f08
ahuerta <angeladrianhp@hotmail.com>
2021-12-18 19:55:10 -0600

    Dynamic render components

--- src/components/StepForm/Form.jsx
+++ src/components/StepForm/Form.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-
 import StepWizard from "react-step-wizard";
 
 import NavStep from './NavStep';
-import PersonalData from './PersonalData';
-import EmailData from './EmailData';
-import DepartmentData from './DepartmentData';
-import FloorData from './FloorData';
-import ComfortData from './ComfortData';
-import ParkingData from './ParkingData';
-import PriceData from './PriceData';
-import ImageData from './ImageData';
-import ElevatorData from './ElevatorData';
-import ResumeData from './ResumeData';
+import render from './components';
+
+import data from '../../formData.json';
 
 let custom = {
   enterRight: '',
@@ -23,21 +15,15 @@ let custom = {
 }
 
 function Form() {
+
   return (
     <div className="card">
       <div className="card-body">
       <form>
         <StepWizard isHashEnabled transitions={custom} nav={<NavStep />}>
-          <PersonalData hashKey="informacion-personal" />
-          <EmailData hashKey="email" />
-          <DepartmentData hashKey="departamento" />
-          <FloorData hashKey="piso-departamento" />
-          <ComfortData hashKey="comodidades" />
-          <ParkingData hashKey="parqueadero" />
-          <PriceData hashKey="precio-departamento" />
-          <ImageData hashKey="imagen-departamento" />
-          <ElevatorData hashKey="ascensor" />
-          <ResumeData hashKey="resumen" />
+        {
+          data.form.map(blocks => render(blocks))
+        }
         </StepWizard>
       </form>
       </div>
---
+++ src/components/StepForm/components.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import PersonalData from './PersonalData';
+import EmailData from './EmailData';
+import DepartmentData from './DepartmentData';
+import FloorData from './FloorData';
+import ComfortData from './ComfortData';
+import ParkingData from './ParkingData';
+import PriceData from './PriceData';
+import ImageData from './ImageData';
+import ElevatorData from './ElevatorData';
+import ResumeData from './ResumeData';
+
+const components = {
+  PersonalData: PersonalData,
+  EmailData: EmailData,
+  DepartmentData: DepartmentData,
+  FloorData: FloorData,
+  ComfortData: ComfortData,
+  ParkingData: ParkingData,
+  PriceData: PriceData,
+  ImageData: ImageData,
+  ElevatorData: ElevatorData,
+};
+
+function render(data) {
+  return React.createElement(
+    components[data.component],
+    {
+      key: data.order,
+      hashKey: data.hash,
+    }
+  );
+}
+
+export default render;
