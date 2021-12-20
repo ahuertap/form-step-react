c77ce92feae5a74b34101c9133beca8dee8a941d
ahuerta <angeladrianhp@hotmail.com>
2021-12-20 12:32:46 -0600

    Add redux to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
 
+import reducer from './reducers';
 import App from './routes/App';
+
+const middleware = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(...middleware),
+));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
