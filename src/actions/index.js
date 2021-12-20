2407a88ba3fbf48b076b9b7231490aad1feb8d02
ahuerta <angeladrianhp@hotmail.com>
2021-12-20 12:33:05 -0600

    Add reducers and actions for components

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+export const setName = (payload) => ({
+  type: 'SET_NAME',
+  payload,
+});
+
+export const setEmail = (payload) => ({
+  type: 'SET_EMAIL',
+  payload,
+});
+
+export const setDepartment = (payload) => ({
+  type: 'SET_DEPARTMENT',
+  payload,
+});
+
+export const setFloor = (payload) => ({
+  type: 'SET_FLOOR',
+  payload,
+});
+
+export const setPrice = (payload) => ({
+  type: 'SET_PRICE',
+  payload,
+});
+
+export const setParking = (payload) => ({
+  type: 'SET_PARKING',
+  payload,
+});
+
+export const setElevator = (payload) => ({
+  type: 'SET_ELEVATOR',
+  payload,
+});
+
+export const setImage = (payload) => ({
+  type: 'SET_IMAGE',
+  payload,
+});
+
+export const setComfort = (payload) => ({
+  type: 'SET_COMFORT',
+  payload,
+});
+
+export const deleteComfort = (payload) => ({
+  type: 'DELETE_COMFORT',
+  payload,
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,70 @@
+const initialState = {
+  name: null,
+  email: null,
+  department: null,
+  floor: null,
+  comfort: [],
+  parking: null,
+  price: null,
+  image: null,
+  elevator: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case 'SET_EMAIL':
+      return {
+        ...state,
+        email: action.payload,
+      };
+    case 'SET_DEPARTMENT':
+      return {
+        ...state,
+        department: action.payload,
+      };
+    case 'SET_FLOOR':
+      return {
+        ...state,
+        floor: action.payload,
+      };
+    case 'SET_PRICE':
+      return {
+        ...state,
+        price: action.payload,
+      };
+    case 'SET_PARKING':
+      return {
+        ...state,
+        parking: action.payload,
+      };
+    case 'SET_ELEVATOR':
+      return {
+        ...state,
+        elevator: action.payload,
+      };
+    case 'SET_IMAGE':
+      return {
+        ...state,
+        image: action.payload,
+      };
+    case 'SET_COMFORT':
+      return {
+        ...state,
+        comfort: [...state.comfort, action.payload],
+      };
+    case 'DELETE_COMFORT':
+      return {
+        ...state,
+        comfort: state.comfort.filter((item) => item !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
